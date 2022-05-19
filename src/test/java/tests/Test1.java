c8bc7049164ac1e5642849bd0cf89a38d0e1abcc
Ozer <ozerknet@gmail.com>
2022-05-19 14:14:02 +0100

    Selenium Day 6-7 repeat

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -9,6 +10,7 @@ import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Test1 {
@@ -180,9 +182,206 @@ public class Test1 {
     }
 
     @Test
-    public void a(){
+    public void radioButtons() {
+        driver.get("https://practice.cydeo.com/radio_buttons");
+        WebElement hockeyRadioBtn = driver.findElement(By.xpath("//input[@id='hockey']"));
+        hockeyRadioBtn.click();
+        if (hockeyRadioBtn.isSelected()) {
+            System.out.println("Button is selected. Verification PASSED!");
+        } else {
+            System.out.println("Button is not selected. Verification FAILED!!!");
+        }
+    }
+
+/*
+    @Test
+    public void radioButtonsWithMethod() {
+        driver.get("https://practice.cydeo.com/radio_buttons");
+
+        //Locate name='sports' radio buttons and store them in a List of Web Element
+
+        clickAndVerifyRadioButton(driver, "sport", "hockey");
+
+        clickAndVerifyRadioButton(driver, "sport", "football");
+
+        clickAndVerifyRadioButton(driver, "color", "yellow");
+
+        private static void clickAndVerifyRadioButton (WebDriver driver, String nameAttribute, String idValue){
+
+            List<WebElement> radioButtons = driver.findElements(By.name(nameAttribute));
+
+            //Loop through the List of WebElement and select matching result "hockey"
+            for (WebElement each : radioButtons) {
+                String eachId = each.getAttribute("id");
+
+                if (eachId.equals(idValue)) {
+
+                    each.click();
+                    System.out.println(eachId + " is selected : " + each.isSelected());
+                    break;
+                }
+            }
+
+        }
+    }
+*/ // radioButton With Method ???
+
+    @Test
+    public void dropDown() {
+        driver.get("https://practice.cydeo.com/dropdown");
+
+        Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+        WebElement currentlySelectedOption = simpleDropdown.getFirstSelectedOption();
+        String actualSimpleDropdownText = currentlySelectedOption.getText();
+        String expectedSimpleDropdownText = "Please select an option";
+        Assert.assertEquals(actualSimpleDropdownText, expectedSimpleDropdownText);
+
+
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+        String expectedStateDropdownText = "Select a State";
+        String actualStateDropdownText = stateDropdown.getFirstSelectedOption().getText();
+        Assert.assertEquals(actualStateDropdownText, expectedStateDropdownText);
 
     }
+
+
+    @Test
+    public void dropDownMorePractice() {
+        driver.get("https://practice.cydeo.com/dropdown");
+        Select yearDropdown = new Select(driver.findElement(By.xpath("//select[@id='year']")));
+        Select monthDropdown = new Select(driver.findElement(By.xpath("//select[@id='month']")));
+        Select dayDropdown = new Select(driver.findElement(By.xpath("//select[@id='day']")));
+
+        yearDropdown.selectByVisibleText("1980");
+        monthDropdown.selectByVisibleText("April");
+        dayDropdown.selectByIndex(3);
+
+        String expectedYear = "1980";
+        String expectedMonth = "April";
+        String expectedDay = "4";
+
+        String actualYear = yearDropdown.getFirstSelectedOption().getText();
+        String actualMonth = monthDropdown.getFirstSelectedOption().getText();
+        String actualDay = dayDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertTrue(actualYear.equals(expectedYear));
+        Assert.assertEquals(actualMonth, expectedMonth);
+        Assert.assertEquals(actualDay, expectedDay);
+
+
+    }
+
+    @Test
+    public void Alerts() {
+        driver.get("https://practice.cydeo.com/javascript_alerts");
+        WebElement informationAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+
+        informationAlertButton.click();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+        WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
+        Assert.assertTrue(resultText.isDisplayed(), "Result text is NOT displayed.");
+        String expectedText = "You successfully clicked an alert";
+        String actualText = resultText.getText();
+        Assert.assertEquals(actualText, expectedText, "Actual result text is not as expected!!!");
+
+    }
+
+
+    @Test
+    public void iFrames() {
+        driver.get("https://practice.cydeo.com/iframe");
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']")));
+        WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
+
+        Assert.assertTrue(yourContentGoesHereText.isDisplayed());
+
+        driver.switchTo().parentFrame();
+        WebElement headerText = driver.findElement(By.xpath("//h3"));
+        Assert.assertTrue(headerText.isDisplayed());
+
+    }
+
+    @Test
+    public void windows() {
+        driver.get("https://practice.cydeo.com/windows");
+        String mainHandle = driver.getWindowHandle();
+        System.out.println("mainHandle = " + mainHandle);
+
+        String expectedTitle = "Windows";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+        System.out.println("Title before click: " + actualTitle);
+
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+        clickHereLink.click();
+
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        for(String each: allWindowHandles){
+
+            driver.switchTo().window(each);
+            System.out.println("Current title while switching windows: " + driver.getTitle());
+
+        }
+
+        String expectedTitleAfterClick = "New Window";
+        actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitleAfterClick);
+        System.out.println("Title after click: " + actualTitle);
+
+
+    }
+
+    @Test
+    public void xx() {
+        driver.get("https://practice.cydeo.com/");
+
+    }
+
+    @Test
+    public void xxx() {
+        driver.get("https://practice.cydeo.com/");
+
+    }
+
+    @Test
+    public void xxxx() {
+        driver.get("https://practice.cydeo.com/");
+
+    }
+
+    @Test
+    public void xxxxx() {
+        driver.get("https://practice.cydeo.com/");
+
+    }
+
+    @Test
+    public void xx1() {
+        driver.get("https://practice.cydeo.com/");
+
+    }
+
+    @Test
+    public void xxx1() {
+        driver.get("https://practice.cydeo.com/");
+
+    }
+
+    @Test
+    public void xxxx1() {
+        driver.get("https://practice.cydeo.com/");
+
+    }
+
+    @Test
+    public void xxxxx1() {
+        driver.get("https://practice.cydeo.com/");
+
+    }
+
+
 
     /*
     Radio Button
@@ -199,5 +398,5 @@ public class Test1 {
         // driver.quit();
     }
 
-
 }
+
